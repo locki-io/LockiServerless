8b6068244b4538b01d54dcee5106bc17725677ae
satishn92 <nvrn.satish@gmail.com>
2023-10-11 23:21:12 +0530

    Uses Itheum sdk to decode token

--- src/services/getDataNfts.ts
+++ src/services/getDataNfts.ts
@@ -3,8 +3,10 @@ import { DataNftRepository, NativeAuthRepository } from '../repository';
 import { NftType } from '@multiversx/sdk-dapp/types/tokens.types';
 import { DataNftMetadataType } from '../types/dataNftTypes';
 import { DynamoDBRepository } from '../awsServiceRepository';
+import { DataNft } from '@itheum/sdk-mx-data-nft';
 
 const dbClient = new DynamoDBRepository(process.env.LOCKI_USERS_TABLE || '');
+DataNft.setNetworkConfig('devnet');
 
 export const getDataNftsService = async (event: APIGatewayProxyEvent) => {
   const { address, apiKey } = event?.queryStringParameters || {};
@@ -14,9 +16,11 @@ export const getDataNftsService = async (event: APIGatewayProxyEvent) => {
 
   const filteredNfts: NftType[] = dataNfts.filter((nft) => nft.collection === process.env.NFT_COLLECTION);
 
-  const decodedNfts: DataNftMetadataType[] = filteredNfts.map((nft) => {
-    const decodedNft = DataNftRepository.decodeNftAttributes(nft);
-    return decodedNft;
-  });
+  const decodedNfts: DataNft[] = await Promise.all(
+    filteredNfts.map(async (nft: NftType) => {
+      const decodedNft = await DataNft.createFromApi({ nonce: nft.nonce });
+      return decodedNft;
+    }),
+  );
   return decodedNfts;
 };
